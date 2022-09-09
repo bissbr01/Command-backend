de9c84e7685b6edfae09b06e54b9f13d18bcb40a
Brad Bissell <bissell.brad@gmail.com>
2022-09-08 21:31:01 -0400

    fixing heroku build crashing on jwt auth config

--- util/authorizeUser.js
+++ util/authorizeUser.js
@@ -3,8 +3,9 @@ const { AUDIENCE, ISSUER_BASE_URL } = require('./config');
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
 const checkJwt = auth({
-  issuerBaseURL: ISSUER_BASE_URL,
   audience: AUDIENCE,
+  issuerBaseURL: ISSUER_BASE_URL,
+  jwksUri: 'https://dev-w8p6njku.us.auth0.com/.well-known/jwks.json',
 });
 
 module.exports = { checkJwt, requiredScopes };
